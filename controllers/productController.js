91c977e9c146e55ec812fc6f71156bbdcb97a8bc
divice01 <dmacias49@calstela.edu>
2025-06-09 14:42:44 -0700

    Commit

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,13 +1,13 @@
-import axios from 'axios';
+import axios from "axios";
 
 const API_BASE = process.env.NEXT_PUBLIC_API_BASE;
 
 export const fetchAllProducts = async () => {
-    try{
-        const res = await axios.get(`${API_BASE}/products`); 
-        return res.data;
-    }catch (error) {
-        console.error(" Error fetching products:" , error.message);
-        throw new Error("Failed to fetch products. Please try again later.");
-    }
-}
+  try {
+    const res = await axios.get(`https://back-end-ecom.onrender.com/products`);
+    return res.data;
+  } catch (error) {
+    console.error(" Error fetching products:", error.message);
+    throw new Error("Failed to fetch products. Please try again later.");
+  }
+};
